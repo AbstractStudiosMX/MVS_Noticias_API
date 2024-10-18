86326eb38cd1c7d149d194cfda8075ece86e3d50
Marcos Valdez <mvr74405@udelasalle.edu.mx>
2024-10-18 09:15:48 -0600

    currency controller
    -added full requirement for currency endpoint

---
+++ MVS_Noticias_API/Controllers/CurrencyController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MVS_Noticias_API.Models.Currency;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace MVS_Noticias_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class CurrencyController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+        private readonly ILogger _logger;
+
+        public CurrencyController(IConfiguration configuration, ILogger<AuthenticationController> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        [HttpGet("convert")]
+        public async Task<ActionResult<Currency>> ConvertCurrency(string from, string to)
+        {
+            try
+            {
+                var apiKey = _configuration.GetSection("AppSettings:CurrencyApiKey").Value;
+                var amount = 1; // amount to convert (default 1)
+
+                var httpClient = new HttpClient();
+
+                // Solicitud de conversión de divisas actual
+                var responseCurrencyConvertion = await httpClient.GetAsync($"https://api.finage.co.uk/convert/forex/{from}/{to}/{amount}?apikey={apiKey}");
+                responseCurrencyConvertion.EnsureSuccessStatusCode();
+                var currencyConvertionContent = await responseCurrencyConvertion.Content.ReadAsStringAsync();
+                var currencyConvertion = JsonSerializer.Deserialize<CurrencyResponse>(currencyConvertionContent);
+
+                // Solicitud de precio de cierre del día anterior
+                var responsePreviousClose = await httpClient.GetAsync($"https://api.finage.co.uk/agg/forex/prev-close/{from}{to}?apikey={apiKey}");
+                responsePreviousClose.EnsureSuccessStatusCode();
+                var previousCloseContent = await responsePreviousClose.Content.ReadAsStringAsync();
+                var previousCloseResponse = JsonSerializer.Deserialize<PreviousCloseResponse>(previousCloseContent);
+                var previousClose = previousCloseResponse.results[0].c;
+
+                // Calcular cambio absoluto
+                var absoluteChange = currencyConvertion.value - previousClose;
+
+                // Calcular porcentaje de crecimiento
+                var growthPercentage = ((currencyConvertion.value - previousClose) / previousClose) * 100;
+
+                var currencyData = new Currency
+                {
+                    From = from,
+                    To = to,
+                    ExchangeRate = currencyConvertion.value,
+                    AbsoluteChange = absoluteChange,
+                    GrowthPercentage = growthPercentage,
+                    Timestamp = currencyConvertion.timestamp,
+                };
+
+                return Ok(currencyData);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error getting currency data: " + ex.Message);
+                return BadRequest("Error getting currency data.");
+            }
+        }
+    }
+}
---
+++ MVS_Noticias_API/Models/Currency/Currency.cs
@@ -0,0 +1,35 @@
+﻿namespace MVS_Noticias_API.Models.Currency
+{
+    public class Currency
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public decimal ExchangeRate { get; set; }
+        public decimal AbsoluteChange { get; set; }
+        public decimal GrowthPercentage { get; set; }
+        public long Timestamp { get; set; }
+    }
+
+    public class CurrencyResponse
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public decimal value { get; set; }
+        public long timestamp { get; set; }
+    }
+
+    public class PreviousCloseResponse
+    {
+        public List<PreviousCloseResult> results { get; set; }
+    }
+
+    public class PreviousCloseResult
+    {
+        public decimal o { get; set; }  // Open
+        public decimal h { get; set; }  // High
+        public decimal l { get; set; }  // Low
+        public decimal c { get; set; }  // Close
+        public int v { get; set; }      // Volume
+        public long t { get; set; }     // Timestamp
+    }
+}
